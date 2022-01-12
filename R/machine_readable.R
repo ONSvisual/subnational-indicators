15fbb59ab2d4d211253ff4b5783e5fcfbcf7a337
henryjameslau <henry.james.lau@gmail.com>
2022-01-12 17:12:35 +0000

    Update to include all LAs from 2009-2021 for machine readable

--- R/machine_readable.R
+++ R/machine_readable.R
@@ -7,67 +7,94 @@ library(tidyverse) ; library(sf) ; library(readxl)
 # Local authority districts (2021)
 # Source: ONS Open Geography Portal
 # URL: https://geoportal.statistics.gov.uk/datasets/local-authority-districts-may-2021-uk-buc
-ltla <- read_csv("geospatial/Local_Authority_Districts_2021.csv") %>% 
-  select(AREACD = LAD21CD, AREANM = LAD21NM) %>% 
+ltla09 <- read_csv("geospatial/Local_Authority_Districts_(December_2009)_Boundaries.csv") %>%
+  select(AREACD = lad09cd, AREANM = lad09nm) %>%
   mutate(Tier = "District/Unitary")
+
+ltla11 <- read_csv("geospatial/Local_Authority_Districts_(December_2011)_Boundaries_EW_BGC.csv") %>%
+  select(AREACD = lad11cd, AREANM = lad11nm) %>%
+  mutate(Tier = "District/Unitary")
+
+ltla17 <- read_csv("geospatial/Local_Authority_Districts__December_2017__Boundaries_GB_BUC.csv") %>%
+  select(AREACD = LAD17CD, AREANM = LAD17NM) %>%
+  mutate(Tier = "District/Unitary")
+
+ltla18 <- read_csv("geospatial/Local_Authority_Districts_(December_2018)_Boundaries_UK_BUC.csv") %>%
+  select(AREACD = lad18cd, AREANM = lad18nm) %>%
+  mutate(Tier = "District/Unitary")
+
+ltla19 <- read_csv("geospatial/Local_Authority_Districts_(December_2019)_Boundaries_UK_BUC.csv") %>%
+  select(AREACD = lad19cd, AREANM = lad19nm) %>%
+  mutate(Tier = "District/Unitary")
+
+ltla20 <- read_csv("geospatial/Local_Authority_Districts_(December_2020)_UK_BUC.csv") %>%
+  select(AREACD = LAD20CD, AREANM = LAD20NM) %>%
+  mutate(Tier = "District/Unitary")
+
+ltla21 <- read_csv("geospatial/Local_Authority_Districts_2021.csv") %>%
+  select(AREACD = LAD21CD, AREANM = LAD21NM) %>%
+  mutate(Tier = "District/Unitary")
+
+ltla <- bind_rows(ltla09,ltla11,ltla17,ltla18,ltla19,ltla20,ltla21) %>% distinct(AREACD,.keep_all=TRUE)
 
 # Counties (2021)
 # Source: ONS Open Geography Portal
 # URL: https://geoportal.statistics.gov.uk/datasets/counties-april-2021-names-and-codes-in-england
-counties <- read_csv("geospatial/Counties_2021.csv") %>% 
-  select(AREACD = CTY21CD, AREANM = CTY21NM) %>% 
+counties <- read_csv("geospatial/Counties_2021.csv") %>%
+  select(AREACD = CTY21CD, AREANM = CTY21NM) %>%
   mutate(Tier = "County/Unitary")
 
 tiers <- bind_rows(ltla, counties)
 
 # Metrics
-metadata <- read_xlsx("MetricsData.xlsx", sheet = "Metadata")
-
 path <- "MetricsData.xlsx"
 
-sheets <- path %>% 
-  excel_sheets() %>% 
-  set_names() %>% 
-  .[.!= "Metadata"] 
+metadata <- read_xlsx(path, sheet = "Metadata")
 
-raw <- map_df(sheets, ~mutate(read_excel(path, sheet = .x, 
-                                         col_types = c("text", "numeric"), 
+sheets <- path %>%
+  excel_sheets() %>%
+  set_names() %>%
+  .[.!= "Metadata"]
+
+raw <- map_df(sheets, ~mutate(read_excel(path, sheet = .x,
+                                         col_types = c("text", "numeric"),
                                          skip = 1), Worksheet = .x))
 
 # Clean data ---------------------------------
 
 # enrich with tier information
-raw_geo <- left_join(raw, tiers, by = "AREACD") %>% 
+raw_geo <- left_join(raw, tiers, by = "AREACD") %>%
   filter(!is.na(Tier))
 
 # filter metrics with "District/Unitary" and remove "County/Unitary"
 a <- filter(raw_geo, Worksheet %in% pull(distinct(filter(raw_geo, AREANM == "Fareham")), Worksheet) &
-           Tier != "County/Unitary") %>% 
+           Tier != "County/Unitary") %>%
   mutate(Tier == "District/Unitary")
 
 # filter metrics with "County/Unitary"
-b <- filter(raw_geo, !Worksheet %in% pull(distinct(filter(raw_geo, AREANM == "Fareham")), Worksheet)) %>% 
+b <- filter(raw_geo, !Worksheet %in% pull(distinct(filter(raw_geo, AREANM == "Fareham")), Worksheet)) %>%
   mutate(Tier == "County/Unitary")
 
 raw_geo_final <- bind_rows(a, b)
 
-# pull in metadata 
+# pull in metadata
 # cannot standardise across different tiers!!
-df <- map_df(pull(distinct(raw_geo, Worksheet)), ~raw_geo %>% 
          filter(Worksheet == .x) %>% 
+df <- map_df(pull(distinct(raw_geo, Worksheet)), ~raw_geo %>%
+         filter(Worksheet == .x) %>%
          mutate(Indicator = pull(filter(metadata, Worksheet == .x), Indicator),
                 Shortened = pull(filter(metadata, Worksheet == .x), Shortened),
                 Category = pull(filter(metadata, Worksheet == .x), Category),
                 Period = pull(filter(metadata, Worksheet == .x), Period),
                 Measure = pull(filter(metadata, Worksheet == .x), Measure),
-                Unit = pull(filter(metadata, Worksheet == .x), Unit)) %>% 
-           relocate(Value, .after = Unit)) %>% 
+                Unit = pull(filter(metadata, Worksheet == .x), Unit)) %>%
+           relocate(Value, .after = Unit)) %>%
   mutate(Period = case_when(
     Worksheet == "HouseAdditions" & str_detect(AREACD, "^N") ~ "2021",
     Worksheet == "HouseAdditions" & str_detect(AREACD, "^W|^S") ~ "2020",
     Worksheet == "HouseAdditions" & str_detect(AREACD, "^E") ~ "2019-20",
-    TRUE ~ Period)) %>% 
+    TRUE ~ Period)) %>%
   select(-Worksheet)
 
 # Write data ---------------------------------
-write_excel_csv(select(df, -Shortened), "machine_readable.csv")
+write_excel_csv(select(df, -c(Shortened)), "machine_readable.csv")
