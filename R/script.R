05f2a8a0aae5e3b422b217a6a887f10ddf895201
rcatlord <hcpartridge@uclmail.net>
2022-08-22 17:03:36 +0100

    Update script.R

--- R/script.R
+++ R/script.R
@@ -1,55 +1,52 @@
 # Data for jitter plot #
 
-library(tidyverse) ; library(sf) ; library(readxl)
+library(tidyverse) ; library(readxl)
+
+# Read data ------------------------------
+
+# Metadata and indicators
+# Source: Open Government data
+# URL: https://github.com/ONSdigital/LUDA
+metadata <- read_csv("metadata.csv")
+files <- list.files(path = "C:/Users/partrh/Downloads/LUDA-main/Output", pattern = "*.csv", full.names = TRUE)
+raw <- files %>%
+  setNames(nm = .) %>% 
+  map_df(~read_csv(.x, col_types = cols(.default = "c")), .id = "Worksheet") %>%
+  mutate(Worksheet = str_extract(Worksheet, "(?<=Output/)(.+)(?=\\.)")) %>% 
+  # exclude indicators that aren't to be normalised
+  filter(!Worksheet %in% pull(filter(metadata, Jitter == "exclude"), Worksheet))
 
 # Local Authority districts (2021)
 # Source: ONS Open Geography Portal
 # URL: https://geoportal.statistics.gov.uk/datasets/local-authority-districts-may-2021-uk-buc
-lad <- read_csv("geospatial/Local_Authority_Districts_2021.csv") %>% 
-  select(AREACD = LAD21CD, AREANM = LAD21NM)
+lad <- pull(read_csv("geospatial/Local_Authority_Districts_2021.csv"), LAD21CD)
 
-# Metrics
-path <- "2022_04_22_MetricsData_input.xlsx"
-metadata <- read_xlsx(path, sheet = "Metadata")
+# Clean and create MAD scores ------------------------------
+df <- raw %>% 
+  filter(AREACD %in% lad) %>% # filter by local authority
+  group_by(Worksheet) %>% 
+  filter(Period == max(Period)) %>% # filter by latest period
+  ungroup() %>% 
+  # join metadata
+  select(-Category) %>% 
+  left_join(select(metadata, Worksheet, Shortened, Category), by = "Worksheet") %>% 
+  group_by(Worksheet) %>% 
+  mutate(
+    # reverse polarity
+    Value = as.double(Value),
+    Value = case_when(Polarity == -1 ~Value*(-1), TRUE ~ Value),
+    # calculate median absolute deviation
+    MAD = (Value - median(Value, na.rm = TRUE)) / median(abs(Value - median(Value, na.rm = TRUE))*1.4826, na.rm = TRUE),
+    Value = case_when(Polarity == -1 ~abs(Value), TRUE ~ Value)) %>% 
+  ungroup() %>% 
+  select(unique = AREACD, 
+         group = AREANM, 
+         Geography, Indicator, 
+         id = Shortened, 
+         Category, Period, Measure,
+         unit = Unit, 
+         real = Value,
+         value = MAD) 
 
-sheets <- path %>% 
-  excel_sheets() %>% 
-  set_names() %>% 
-  .[.!= "Metadata"] 
-
-negatives <- pull(filter(metadata, Polarity == "Negative"), Worksheet)
-
-raw <- left_join(lad,
-                 map_df(sheets, ~mutate(read_excel(path, sheet = .x, col_types = c("text", "numeric","text"), skip = 1), Worksheet = .x)),
-                 by = "AREACD")
-
-df <- map_df(pull(distinct(raw, Worksheet)), ~raw %>% 
-         filter(Worksheet == .x) %>% 
-         mutate(Indicator = pull(filter(metadata, Worksheet == .x), Indicator),
-                Shortened = pull(filter(metadata, Worksheet == .x), Shortened),
-                Category = pull(filter(metadata, Worksheet == .x), Category),
-                Period = pull(filter(metadata, Worksheet == .x), Period),
-                Measure = pull(filter(metadata, Worksheet == .x), Measure),
-                Unit = pull(filter(metadata, Worksheet == .x), Unit),
-                # reverse polarity
-                Value = case_when(Worksheet %in% negatives ~Value*(-1),
-                                  TRUE ~ Value),
-                # median absolute deviation
-                MAD = (Value - median(Value, na.rm = TRUE)) / median(abs(Value - median(Value, na.rm = TRUE))*1.4826, na.rm = TRUE),
-                Value = case_when(Worksheet %in% negatives ~abs(Value),
-                                  TRUE ~ Value)) %>% 
-           relocate(Value, .after = Unit)) %>% 
-  select(-Worksheet) %>% 
-  # exlude metrics that aren't normalised
-  filter(!Indicator %in% pull(filter(metadata, Jitter == "exclude"), Indicator))
-
-results <- rename(df, 
-                  unique = AREACD,
-                  group = AREANM,
-                  id = Shortened,
-                  unit = Unit,
-                  real = Value,
-                  value = MAD)
-
-# Write results
-write_excel_csv(results, "../app/revised_data.csv")
+# Write results ------------------------------
+write_excel_csv(df, "../app/revised_data.csv")
